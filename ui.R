2b115ceec9c94e0279697d76ce1ac6c9e06dd25b
mlyasheva <36286040+mlyasheva@users.noreply.github.com>
2019-02-28 12:57:05 +0000

    Update ui.R

--- ui.R
+++ ui.R
@@ -15,7 +15,11 @@ ui <- fluidPage(
                         selectInput("group", "Population group:", 
                                     choices=colnames(VADeaths)),
                         hr(),
-                        helpText("Description: Death rates per 1000 in Virginia in 1940.")
+                        helpText("Description: Death rates per 1000 in Virginia in 1940.
+                                 The death rates are measured per 1000 population per year.
+                                 They are classified by age group (50–54, 55–59, 60–64, 65–69, 70–74)
+                                 and population group (Rural/Male, Rural/Female, Urban/Male and Urban/Female).
+                                 You can select a population group above in order to get statistics about the death rates in Virginia in 1940 by age group.")
                 ),
                 
                 # Create a spot for the barplot
